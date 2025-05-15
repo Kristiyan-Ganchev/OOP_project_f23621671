cd8e6a256f020445b2609a505c2b341a7127cf97
Kristiyan Ganchev <60877374+Kristiyan-Ganchev@users.noreply.github.com>
2025-05-15 11:23:35 +0300

    Create BadDataException.java
    ...

---
+++ src/bg/tu_varna/sit/a1/f23621671/Exceptions/BadDataException.java
@@ -0,0 +1,7 @@
+package bg.tu_varna.sit.a1.f23621671.Exceptions;
+
+public class BadDataException extends Exception{
+    public BadDataException(String message) {
+        super(message);
+    }
+}
